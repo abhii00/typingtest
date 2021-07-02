94339789cf6d9d0b1422d0b6f71e1babf6fc16e4
abhi00 <abhijit.pandit808@gmail.com>
2021-07-02 04:05:06 +0100

    entry update functionality

--- src/index.js
+++ src/index.js
@@ -87,7 +87,7 @@ function Counter(props){
 class Entry extends React.Component{
     render(){
         return(
-            <input type="text" className="entry"></input>
+            <input type="text" className="entry" onChange={this.props.handleChange}></input>
         );
     }
 }
@@ -99,8 +99,38 @@ class TypingTest extends React.Component{
             dictionarySource: "./dictionaries/dict_test.txt",
             wordsCorrect: 5,
             wordsTotal: 200,
+
             timeRemaining: 30,
+            startTime: 0,
+            currentTime: 0,
+            endTime: 0,
+
+            testing: "pre",
+            currentEntry: ""
         };
+
+        this.consts = {
+            wordsTotal: 200,
+
+            testTime: 60,
+            intervalTime: 1
+        }
+
+        this.startTest = this.startTest.bind(this);
+        this.entryUpdate = this.entryUpdate.bind(this);
+    }
+
+    startTest(){
+        this.setState({
+            testing: "test",
+            startTime: Date.now(),
+            endTime: this.state.startTime + this.state.testTime
+        });
+        console.log("Test Started");
+    }
+
+    entryUpdate(e){
+        this.setState({currentEntry: e.target.value});
     }
 
     renderCounter(){
@@ -112,19 +142,37 @@ class TypingTest extends React.Component{
     }
 
     renderWordDisplay(){
-        return <WordDisplay dictionarySource={this.state.dictionarySource} wordsTotal={this.state.wordsTotal}/>;
+        return <WordDisplay dictionarySource={this.state.dictionarySource} wordsTotal={this.consts.wordsTotal}/>;
     }
 
     render() {
-        return(
-            <div>
-                <Title/>
-                {this.renderCounter()}
-                {this.renderTimer()}
-                {this.renderWordDisplay()}
-                <Entry/>
-            </div>
-        );
+        switch (this.state.testing){
+            case "pre":
+                return (
+                    <div>
+                        <Title/>
+                        {this.renderCounter()}
+                        {this.renderTimer()}
+                        {this.renderWordDisplay()}
+                        <Entry handleChange={this.startTest}/>
+                    </div>
+                );
+            case "test":
+                return (
+                    <div>
+                        <Title/>
+                        {this.renderCounter()}
+                        {this.renderTimer()}
+                        {this.renderWordDisplay()}
+                        <Entry handleChange={this.entryUpdate}/>
+                        <button onClick = {() => this.setState({testing: "pre"})}> Stop </button>
+                    </div>
+                );
+            case "post":
+                break;
+            default:
+                break;
+        }
     }
 }
 
